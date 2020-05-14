6451ed0ffefd10c8b7061cf077d6341214b15492
Gabe Sumner <gsumner@gsumner-ltm8.internal.salesforce.com>
2020-05-14 16:58:01 -0700

    Added navigation to Archive Case

--- force-app/main/default/lwc/archiveCase/archiveCase.js
+++ force-app/main/default/lwc/archiveCase/archiveCase.js
@@ -1,20 +1,55 @@
 import { LightningElement, api, wire } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+import { NavigationMixin } from 'lightning/navigation';
 import callArchiveCase from '@salesforce/apex/archiveController.archiveCase';
 
-export default class ArchiveCase extends LightningElement {
+export default class ArchiveCase extends NavigationMixin(LightningElement) {
     @api recordId;
+    showArchive = true;
+    showSuccess = false;
 
-    buttonClick(event) {
-        console.log('button click:' + this.recordId);
+    @wire(getRecord, { recordId: '$recordId', fields: ['Case.AccountId', 'Case.ContactId'] })
+    record;
+
+    archiveClick(event) {
+        console.log('archiveClick click:' + this.recordId);
 
         callArchiveCase({
             'recordId': this.recordId
         })
         .then(result => {
+            this.showArchive = false;
+            this.showSuccess = true;
             console.log(JSON.stringify(result));
         })
         .catch(error => {
             console.log('ERROR: ' + JSON.stringify(error));
         });        
+    }
+
+    navigateToAccount(event) {
+        console.log(JSON.stringify(this.record));
+        console.log('navigateToAccount: ' + this.record.data.fields.AccountId.value)
+
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: this.record.data.fields.AccountId.value,
+                actionName: 'view'
+            }
+        });
+    }
+
+    navigateToContact(event) {
+        console.log(JSON.stringify(this.record));
+        console.log('navigateToContact: ' + this.record.data.fields.ContactId.value)
+
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: this.record.data.fields.ContactId.value,
+                actionName: 'view'
+            }
+        });
     }
 }
